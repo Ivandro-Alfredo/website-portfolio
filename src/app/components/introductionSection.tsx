45b78fe092abd55befb80343304010660ac08e9c
Ivandro Alfredo <ivandroalfredo10@gmail.com>
2025-11-07 22:57:13 +0100

    feat: new build

--- src/app/components/introductionSection.tsx
+++ src/app/components/introductionSection.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { Animation } from './animation';
 import { FaLinkedin, FaGithub, FaGlobe } from 'react-icons/fa';
 
+
 const Introduction = () => {
 	return (
 		<section>
